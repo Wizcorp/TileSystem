17a2dcc6960a22d3b619176187e6c35101b6eb1e
Rob Towell <rob.towell.development@live.co.uk>
2016-07-31 15:56:40 +0900

    Solver interface

---
+++ TileSystem/Interfaces/Base/IEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace TileSystem.Interfaces.Base
+{
+	/// <summary>
+	/// Specifies an entity which can exist in the tile system
+	/// TODO: Expand the properties to be correct
+	/// </summary>
+	public interface IEntity
+	{
+	}
+}
