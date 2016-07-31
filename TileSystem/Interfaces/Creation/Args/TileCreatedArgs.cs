9135cdc961568248a25bcfe1641def86cff78052
Rob Towell <rob.towell.development@live.co.uk>
2016-07-31 17:21:12 +0900

    Updated Creation Args

--- TileSystem/Interfaces/Creation/Args/TileCreatedArgs.cs
+++ TileSystem/Interfaces/Creation/Args/TileCreatedArgs.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using TileSystem.Interfaces.Base;
 
 namespace TileSystem.Interfaces.Creation
 {
+	/// <summary>
+	/// Emited when a tile is created by an object implementing
+	/// ICreateTiles interface
+	/// </summary>
 	public class TileCreatedArgs : EventArgs
 	{
+		public IArea Area { get; private set; }
+		public ITile Tile { get; private set; }
+
+		public TileCreatedArgs(IArea area, ITile tile)
+		{
+			Area = area;
+			Tile = tile;
+		}
+
+		public override string ToString()
+		{
+			return string.Format("[TileCreatedArgs: Area={0}, Tile={1}]", Area, Tile);
+		}
 	}
 }
