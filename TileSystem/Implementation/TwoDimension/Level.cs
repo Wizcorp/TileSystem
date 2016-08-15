dd5a8cd06106f2dd9076d22b80287cf6511a7f1b
Rob Towell <rob.towell.development@live.co.uk>
2016-08-15 11:06:11 +0900

    level file

---
+++ TileSystem/Implementation/TwoDimension/Level.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using TileSystem.Interfaces.Base;
+using TileSystem.Interfaces.Management;
+
+namespace TileSystem.Implementation.TwoDimension
+{
+	public class Level : ILevel
+	{
+		public event EventHandler<AreaAddedArgs> AreaAdded;
+		public event EventHandler<AreaRemovedArgs> AreaRemoved;
+
+		public void Add(IArea area)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Remove(IArea area)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IArea Get(IPosition position)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<IArea> GetNeighbours(IArea area)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
